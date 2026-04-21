69e021d46b3a4e366810b35541933b69089e5e36
angiecreated <angiepdraw@icloud.com>
2026-04-21 02:17:50 +0200

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYBdl7Z1xTkFb__3KfTfXNQXcSoB-UlOc",
+  authDomain: "ramino-pokerato.firebaseapp.com",
+  projectId: "ramino-pokerato",
+  storageBucket: "ramino-pokerato.firebasestorage.app",
+  messagingSenderId: "951425457894",
+  appId: "1:951425457894:web:ce6ecff0c685dee55d6527"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
