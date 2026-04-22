861280e2173392b40ef055ec9ed990291f798d20
angiecreated <angiepdraw@icloud.com>
2026-04-22 02:42:59 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,6 +4,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyCYBdl7Z1xTkFb__3KfTfXNQXcSoB-UlOc",
   authDomain: "ramino-pokerato.firebaseapp.com",
+  databaseURL: "https://ramino-pokerato-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "ramino-pokerato",
   storageBucket: "ramino-pokerato.firebasestorage.app",
   messagingSenderId: "951425457894",
